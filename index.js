a616dc194a52fe9748628c1b160339b0cf4d15bf
Mike Chevett <mchevett@gmail.com>
2014-04-26 14:34:25 -0400

    for reals

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var redis = require('redis');
 
 var redisClient = redis.createClient(
-	process.env.LETTUCE_REDIS_HOST || '127.0.0.1',
-	process.env.LETTUCE_REDIS_PORT || 6379
+	process.env.LETTUCE_REDIS_PORT || 6379,
+	process.env.LETTUCE_REDIS_HOST || '127.0.0.1'
 );
+
+var password = process.env.LETTUCE_REDIS_PASSWORD;
+if (password){
+	redisClient.auth(password);
+}
 
 var lock = require('redis-lock')(redisClient);
 var jsonify = require('redis-jsonify');
